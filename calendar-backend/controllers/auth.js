19f5981169094c23496ab1b8bebb77f2575ed1d6
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-08-09 12:15:49 -0500

    Custom Middlewares

--- calendar-backend/controllers/auth.js
+++ calendar-backend/controllers/auth.js
@@ -1,19 +1,10 @@
 const { response } = require("express");
-const { validationResult } = require('express-validator');
+/* const { validationResult } = require('express-validator'); */
 
 
 const createUser = (req, res = response) => {
 
   const { name, email, password } = req.body;
-
-  //* Manejo de errores
-  const errors = validationResult( req );
-  if ( !errors.isEmpty() ) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped()
-    });
-  }
 
   res.status(201).json({
     ok: true,
@@ -27,14 +18,6 @@ const createUser = (req, res = response) => {
 const loginUser = (req, res = response) => {
 
   const { email, password } = req.body;
-
-  const errors = validationResult( req );
-  if ( !errors.isEmpty() ) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped()
-    });
-  }
 
   res.status(201).json({
     ok: true,
--- calendar-backend/routes/auth.js
+++ calendar-backend/routes/auth.js
@@ -3,6 +3,7 @@
 
 const { Router } = require("express");
 const { check } = require('express-validator');
+const { validarCampos } = require("../middlewares/validar-campos");
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require("./../controllers/auth");
@@ -12,6 +13,7 @@ router.post("/new",
   check('name', 'El nombre es obligatorio').not().isEmpty(),
   check('email', 'El email es obligatorio').isEmail(),
   check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6}),
+  validarCampos
 ]
 ,
 createUser);
@@ -20,6 +22,7 @@ router.post("/",
 [
   check('email', 'El email es obligatorio').isEmail(),
   check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6}),
+  validarCampos
 ], loginUser);
 
 router.get("/renew", renewToken);
