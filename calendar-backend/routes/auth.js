40d476f34c0bd6c931fd77884df6e3117f68fd4f
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-08-09 11:21:00 -0500

    Express Validator

--- calendar-backend/routes/auth.js
+++ calendar-backend/routes/auth.js
@@ -2,13 +2,25 @@
 //* host + /api/auth
 
 const { Router } = require("express");
+const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require("./../controllers/auth");
 
-router.post("/new", createUser);
+router.post("/new",
+[ //* middlewares
+  check('name', 'El nombre es obligatorio').not().isEmpty(),
+  check('email', 'El email es obligatorio').isEmail(),
+  check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6}),
+]
+,
+createUser);
 
-router.post("/", loginUser);
+router.post("/",
+[
+  check('email', 'El email es obligatorio').isEmail(),
+  check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6}),
+], loginUser);
 
 router.get("/renew", renewToken);
 
