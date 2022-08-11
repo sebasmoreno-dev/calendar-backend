f4bc585ba21d139303fb9ac4a15901dfcb4f88be
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-08-11 13:48:27 -0500

    Login User

--- calendar-backend/controllers/auth.js
+++ calendar-backend/controllers/auth.js
@@ -40,17 +40,53 @@ const createUser = async (req, res = response) => {
   }
 };
 
-const loginUser = (req, res = response) => {
+
+
+const loginUser = async (req, res = response) => {
 
   const { email, password } = req.body;
 
-  res.status(201).json({
-    ok: true,
-    msg: "login",
-    email,
-    password,
-  });
+  try {
+
+    const usuario = await Usuario.findOne({ email });
+
+    if ( !usuario ) {
+      return res.status(400).json({
+        ok: false,
+        mng: 'El usuario no existe con ese correo'
+      });
+    }
+
+    //Confirmar los passwords
+
+    const validPassword = bcrypt.compareSync( password, usuario.password);
+
+    if ( !validPassword ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Password incorrecto'
+      });
+    }
+
+    //Generar nuestrp JWT
+
+    res.json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      ok:false,
+      msg: 'Por favor hable con el administrador'
+    })
+  }
+
 };
+
+
+
 
 const renewToken = (req, res = response) => {
   res.json({
