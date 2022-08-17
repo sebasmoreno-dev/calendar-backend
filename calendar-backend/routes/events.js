a838966dbefdd3265034a2df913dae3373773f26
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2022-08-17 12:56:11 -0500

    Validate required fields

--- calendar-backend/routes/events.js
+++ calendar-backend/routes/events.js
@@ -3,6 +3,10 @@
  */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('./../middlewares/validar-campos');
 const { validarJWT } = require('./../middlewares/validar-jwt');
 const { getEventos, createEvent, updateEvento, deleteEvento} = require('./../controllers/events');
 
@@ -16,7 +20,15 @@ router.use( validarJWT );
 router.get('/', getEventos );
 
 //* Crear un nuevo evento
-router.post('/', createEvent);
+router.post('/',
+[
+  check('title','El titulo es obligatorio').not().isEmpty(),
+  check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+  check('end', 'Fecha de finalización es obligatoria').custom( isDate ),
+  validarCampos
+
+],
+createEvent);
 
 //* Actualizar evento
 router.put('/:id', updateEvento);
